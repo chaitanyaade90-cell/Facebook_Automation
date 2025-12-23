1d83acb762d30510a5ba3a6310f5f62bef116851
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-23 11:07:37 +0530

    Nullp2

---
+++ src/test/java/NullPointer_23_12_2025/NullPointer2.java
@@ -0,0 +1,21 @@
+package NullPointer_23_12_2025;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NullPointer2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver d1=null;
+		System.out.println("My Project name is CCM");
+		d1.get("https://www.gmail.com/");
+		
+		System.out.println("Exception on line no 1");
+		System.out.println("Exception on line no 2");
+		System.out.println("Exception on line no 3");
+				
+
+	}
+
+}
