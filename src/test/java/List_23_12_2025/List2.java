d4ab969cd8009e0ee0ab6810c1bdc8573b73fd4d
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-23 08:39:50 +0530

    List2

---
+++ src/test/java/List_23_12_2025/List2.java
@@ -0,0 +1,34 @@
+package List_23_12_2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> names=new ArrayList<>();
+		
+		names.add("Software");
+		names.add("Sanity");
+		names.add("SmokeTest");
+		
+		System.out.println("Names starting with 'S' ");
+		
+		for (int i = 0; i < names.size(); i++) {
+			
+			String name=names.get(i);
+			
+			if(name.startsWith("S")) {
+				System.out.println(name);
+			}
+			
+			
+			
+			
+		}
+
+	}
+
+}
