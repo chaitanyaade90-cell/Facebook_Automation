bcea8c763035c66f4745cc0a16dcfd05b9c85c24
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-23 20:16:28 +0530

    Gett2

---
+++ src/test/java/ArraySelenium_23_12_2025/GetTitle2.java
@@ -0,0 +1,26 @@
+package ArraySelenium_23_12_2025;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GetTitle2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriverManager.chromedriver();
+		
+		ChromeDriver d1= new ChromeDriver();
+		
+		d1.get("https://www.gmail.com/");
+		
+		d1.manage().window().maximize();
+		
+		String title = d1.getTitle();
+		
+		System.out.println("Page title : " + title);
+
+	}
+
+}
