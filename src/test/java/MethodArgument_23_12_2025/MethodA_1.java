4084d8a7b2a334224072bb35b0250551a30c6d3b
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-23 23:10:42 +0530

    Method1

---
+++ src/test/java/MethodArgument_23_12_2025/MethodA_1.java
@@ -0,0 +1,19 @@
+package MethodArgument_23_12_2025;
+
+public class MethodA_1 {
+	
+	public void Greet1() {
+			System.out.println("Hello, Welcome");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MethodA_1 m1 = new MethodA_1();
+		m1.Greet1();
+		
+		
+
+	}
+
+}
---
+++ src/test/java/MethodArgument_23_12_2025/package-info.java
@@ -0,0 +1,1 @@
+package MethodArgument_23_12_2025;
