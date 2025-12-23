d5cadfc6581e01249c26f376366048589872bd0e
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-23 08:47:33 +0530

    ArrayLoop2

---
+++ src/test/java/ForLoop_Package_23_12_2025/ArrayLoop_2.java
@@ -0,0 +1,16 @@
+package ForLoop_Package_23_12_2025;
+
+public class ArrayLoop_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] fruits={"Apple", "Banana", "Guava"};
+		
+		 for (String fruit : fruits) {
+			 System.out.println(fruit);
+		 }
+
+	}
+
+}
