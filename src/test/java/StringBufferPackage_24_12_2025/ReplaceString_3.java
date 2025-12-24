110689ec3c6bf3ca15d72c1d2466e1da2b28bf71
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 14:54:22 +0530

    Replace1

---
+++ src/test/java/StringBufferPackage_24_12_2025/ReplaceString_3.java
@@ -0,0 +1,14 @@
+package StringBufferPackage_24_12_2025;
+
+public class ReplaceString_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer sb = new StringBuffer("I like Java");
+		sb.replace(7, 11, "Python");
+		System.out.println(sb);
+
+	}
+
+}
