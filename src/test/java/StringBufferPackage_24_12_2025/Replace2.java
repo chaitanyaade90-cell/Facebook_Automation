167e4583b02ddb0dd4e3ab082d3eddad5982fbb1
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 15:14:34 +0530

    Replace

---
+++ src/test/java/StringBufferPackage_24_12_2025/Replace2.java
@@ -0,0 +1,16 @@
+package StringBufferPackage_24_12_2025;
+
+public class Replace2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer sb = new StringBuffer("I like QA");
+		
+		sb.replace(9, 11, "SQM");
+		
+		System.out.println(sb);
+
+	}
+
+}
