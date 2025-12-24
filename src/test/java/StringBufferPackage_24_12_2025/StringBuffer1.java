6b034318637489c0dff7c131d93d5b5660a6b307
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 14:29:51 +0530

    StringBuffer1

---
+++ src/test/java/StringBufferPackage_24_12_2025/StringBuffer1.java
@@ -0,0 +1,14 @@
+package StringBufferPackage_24_12_2025;
+
+public class StringBuffer1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer sb = new StringBuffer("Hello");
+		sb.append("World");
+		System.out.println(sb);
+
+	}
+
+}
---
+++ src/test/java/StringBufferPackage_24_12_2025/package-info.java
@@ -0,0 +1,1 @@
+package StringBufferPackage_24_12_2025;
