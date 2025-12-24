d23f37540f2efd4d9d0cd73a591ace5ebb13cbd5
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 14:36:31 +0530

    StringInsert1

---
+++ src/test/java/StringBufferPackage_24_12_2025/InsertString2.java
@@ -0,0 +1,16 @@
+package StringBufferPackage_24_12_2025;
+
+public class InsertString2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer sb = new StringBuffer("Hello World");
+		
+		sb.insert(5, " ");
+		
+		System.out.println(sb);
+
+	}
+
+}
