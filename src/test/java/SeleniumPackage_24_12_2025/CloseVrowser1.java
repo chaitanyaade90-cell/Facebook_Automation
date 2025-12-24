7abb7c3a6be4032ea35b898cdf5752d584baee72
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 17:02:58 +0530

    Closewindow

--- src/test/java/SeleniumPackage_24_12_2025/CloseVrowser1.java
+++ src/test/java/SeleniumPackage_24_12_2025/CloseVrowser1.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class GetTitle1 {
+public class CloseVrowser1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -13,12 +13,12 @@ public class GetTitle1 {
 		
 		ChromeDriver d1 = new ChromeDriver();
 		
-		d1.get("https://www.github.com/");
+		d1.get("https://www.facebook.com");
 		
 		d1.manage().window().maximize();
 		
-		
-		System.out.println(d1.getTitle());
+		d1.close();
+		d1.quit();
 
 	}
 
---
+++ src/test/java/SeleniumPackage_24_12_2025/DropDownHandling3.java
@@ -0,0 +1,28 @@
+package SeleniumPackage_24_12_2025;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+
+public class DropDownHandling3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriverManager.chromedriver();
+		
+		ChromeDriver d1 = new ChromeDriver();
+		
+		d1.get("https://www.facebook.com");
+		
+		d1.manage().window().maximize();
+		
+		Select DropDown = new Select(d1.findElement(By.id("username")));
+		
+		
+
+	}
+
+}
