196d8f8b505f00bf6f8aa2ad3c463aa335384901
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 21:46:51 +0530

    Inheritance2

--- src/test/java/Inheritance_Package_24_12_2025/Multilevel_Inheritance_1.java
+++ src/test/java/Inheritance_Package_24_12_2025/Multilevel_Inheritance_1.java
@@ -2,30 +2,29 @@ package Inheritance_Package_24_12_2025;
 
 class Animal{
 	
-	public void eat() {
+	void eat() {
 		
-		System.out.println("Eat");
+		System.out.println("Eating");
 	}
 }
 
-class Dog extends Animal{
+class Dog extends Animal
+{
 	
-	public void bark() {
+	void Bark() {
+		
 		System.out.println("Barking");
-		
-		
 	}
 }
 
-public class Inheritance2 {
+public class Multilevel_Inheritance_1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Animal a1=new Animal();
-		
 		a1.eat();
-		a1.b
+		a1.eat();
 
 	}
 
