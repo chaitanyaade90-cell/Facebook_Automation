f18ce626849c4969a54c960cd5ea9c897265e329
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 15:20:17 +0530

    DeleteString

---
+++ src/test/java/StringBufferPackage_24_12_2025/DeleteString4.java
@@ -0,0 +1,16 @@
+package StringBufferPackage_24_12_2025;
+
+public class DeleteString4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer sb = new StringBuffer("Hello SQM");
+		
+		sb.delete(5, 11);
+		
+		System.out.println(sb);
+
+	}
+
+}
