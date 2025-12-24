67719df11e0aeb40fc1272213e60dd5a6a7b787d
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 21:33:31 +0530

    Inherit1

---
+++ src/test/java/Inheritance_Package_24_12_2025/Inheritance2.java
@@ -0,0 +1,32 @@
+package Inheritance_Package_24_12_2025;
+
+class Animal{
+	
+	public void eat() {
+		
+		System.out.println("Eat");
+	}
+}
+
+class Dog extends Animal{
+	
+	public void bark() {
+		System.out.println("Barking");
+		
+		
+	}
+}
+
+public class Inheritance2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Animal a1=new Animal();
+		
+		a1.eat();
+		a1.b
+
+	}
+
+}
