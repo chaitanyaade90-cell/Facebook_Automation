05171f0b9296db80baf5f072c1d8e5992b6d8fc4
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 11:08:32 +0530

    List2

---
+++ src/test/java/ListPackage_24_12_2025/List2.java
@@ -0,0 +1,31 @@
+package ListPackage_24_12_2025;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		List<String> names=new ArrayList<>();
+		
+		names.add("ADO");
+		names.add("Azure");
+		names.add("Manual");
+		
+		System.out.println("Names statrting with 'A' ");
+		
+		for (int i = 0; i < names.size(); i++) {
+			
+			String name=names.get(i);
+			
+			if(name.startsWith("A")) {
+				System.out.println(name);
+			}
+			
+		}
+
+	}
+
+}
