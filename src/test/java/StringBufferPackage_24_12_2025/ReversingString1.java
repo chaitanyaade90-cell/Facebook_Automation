e24361f6c3345ade081bdde1726f1e89195db68a
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 15:25:14 +0530

    Replacestring

---
+++ src/test/java/StringBufferPackage_24_12_2025/ReversingString1.java
@@ -0,0 +1,16 @@
+package StringBufferPackage_24_12_2025;
+
+public class ReversingString1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StringBuffer sb = new StringBuffer("Java");
+		
+		sb.reverse();
+		
+		System.out.println(sb);
+
+	}
+
+}
