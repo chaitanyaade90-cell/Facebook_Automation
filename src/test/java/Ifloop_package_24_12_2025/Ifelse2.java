1d7c38f9010ad49a553f104e489fcb62ed8bb45b
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 08:22:19 +0530

    Ifelse2

---
+++ src/test/java/Ifloop_package_24_12_2025/Ifelse2.java
@@ -0,0 +1,19 @@
+package Ifloop_package_24_12_2025;
+
+public class Ifelse2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age = 25;
+		
+		if(age>21) {
+			System.out.println("Eligible for Scholorship");
+		
+		}else {
+			System.out.println("Not Eligible for Voting");
+		}
+	
+	}
+
+}
