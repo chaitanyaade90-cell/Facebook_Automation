e2e64e83b2d2019281cdf8f9697b6b8b5795407f
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 12:07:02 +0530

    loop3

---
+++ src/test/java/Forloop/NestedLoop.java
@@ -0,0 +1,20 @@
+package Forloop;
+
+public class NestedLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 0; i < args.length; i++) {
+			
+			for (int j = 1; j <=3; j++) {
+				
+				System.out.println("* ");
+				
+			}
+			
+		}
+
+	}
+
+}
---
+++ src/test/java/Loop_Through_Array_24_12_2025/ArrayLoop2.java
@@ -0,0 +1,10 @@
+package Loop_Through_Array_24_12_2025;
+
+public class ArrayLoop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
