6b35ac08c33869f215ec2720e8cac750307ad2ad
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 22:59:01 +0530

    ArrayList1

---
+++ src/test/java/ArrayList_Package_24_12_2025/ArrayList1.java
@@ -0,0 +1,22 @@
+package ArrayList_Package_24_12_2025;
+
+import java.util.ArrayList;
+
+public class ArrayList1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> names = new ArrayList<>();
+		
+		names.add("ADO");
+		names.add("Software");
+		names.add("Testing");
+		
+		System.out.println(names);
+		
+		
+
+	}
+
+}
---
+++ src/test/java/ArrayList_Package_24_12_2025/package-info.java
@@ -0,0 +1,1 @@
+package ArrayList_Package_24_12_2025;
