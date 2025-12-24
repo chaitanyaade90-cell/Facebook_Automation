50cb819f055f5cbe7ef029571458a7169b805b15
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-24 23:06:36 +0530

    ArrayList2

---
+++ src/test/java/ArrayList_Package_24_12_2025/ArrayList2.java
@@ -0,0 +1,17 @@
+package ArrayList_Package_24_12_2025;
+
+import java.util.ArrayList;
+
+public class ArrayList2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String>names = new ArrayList<>();
+		
+		names.set(5, "WBT");
+		System.out.println(names);
+
+	}
+
+}
