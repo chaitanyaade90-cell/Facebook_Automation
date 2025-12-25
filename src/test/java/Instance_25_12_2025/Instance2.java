55338c3133abd50d0608f2051971505fa53457fa
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-25 07:57:36 +0530

    Inst2

---
+++ src/test/java/Instance_25_12_2025/Instance2.java
@@ -0,0 +1,38 @@
+package Instance_25_12_2025;
+
+public class Instance2 {
+	
+	int num;
+	String name;
+	int salary;
+	
+	public void ADO() {
+		System.out.println("My num is " + num);
+	}
+	
+	public void ACCESM() {
+		System.out.println("My name is " + name);
+	}
+	
+	public void ASEETM() {
+		System.out.println("My salary is " + salary);
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Instance2 i2 = new Instance2();
+		
+		i2.num=77;
+		i2.name="AXA";
+		i2.salary=90000;
+		
+		i2.ADO();
+		i2.ACCESM();
+		i2.ASEETM();
+
+	}
+
+}
