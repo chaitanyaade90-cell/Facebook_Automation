8a6e70c547fb1a2dc56fd09ec9c354d09d63868b
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-25 22:07:54 +0530

    Array2

---
+++ src/test/java/ArrayInteger_Package_25_12_2025/ArrayIntString2.java
@@ -0,0 +1,20 @@
+package ArrayInteger_Package_25_12_2025;
+
+import java.util.ArrayList;
+
+public class ArrayIntString2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<Integer> number=new ArrayList<>();
+		
+		number.add(10000);
+		number.add(20000);
+		number.add(30000);
+		
+		System.out.println(number);
+
+	}
+
+}
