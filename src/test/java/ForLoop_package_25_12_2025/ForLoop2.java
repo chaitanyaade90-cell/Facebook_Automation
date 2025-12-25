9578988efa9df2a225f93141a2b0cb3ccab72f3a
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-25 21:46:01 +0530

    forloop2

---
+++ src/test/java/ForLoop_package_25_12_2025/ForLoop2.java
@@ -0,0 +1,18 @@
+package ForLoop_package_25_12_2025;
+
+public class ForLoop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (int i = 5; i < 200; i--) {
+			
+			System.out.println(i);
+			
+		}
+		
+	
+
+	}
+
+}
