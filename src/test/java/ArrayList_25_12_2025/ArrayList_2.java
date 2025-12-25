c13e1c8efdf30e77d66bac72ffdc1abbf0310b6e
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-25 14:53:43 +0530

    Array2

---
+++ src/test/java/ArrayList_25_12_2025/ArrayList_2.java
@@ -0,0 +1,21 @@
+package ArrayList_25_12_2025;
+
+import java.util.ArrayList;
+
+public class ArrayList_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayList<String> names = new ArrayList<>();
+		
+		names.add("Tata");
+		names.add("Wokswagon");
+		names.add("Alto");
+		
+		System.out.println(names);
+		
+
+	}
+
+}
