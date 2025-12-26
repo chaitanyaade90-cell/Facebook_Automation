f2388377818bf03d8cdab05367165532a498b60f
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-26 12:45:16 +0530

    dowhile2

---
+++ src/test/java/Do_while_package_26_12_2025/Do_while2.java
@@ -0,0 +1,20 @@
+package Do_while_package_26_12_2025;
+
+public class Do_while2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10;
+		
+		do {
+			System.out.println(i);
+			
+			i++;
+			
+		}while (i<=10);
+		
+
+	}
+
+}
