0f5c443bf3dba28edf0c05b6da110ecbafca071c
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-26 08:59:05 +0530

    Inst2

---
+++ src/test/java/Instance_26_12_2025/Instance2.java
@@ -0,0 +1,39 @@
+package Instance_26_12_2025;
+
+public class Instance2 {
+	
+	int id;
+	String name;
+	int salary;
+	
+	public void ADO() {
+		System.out.println("My id is " + id);
+	}
+	
+	public void Software() {
+		System.out.println("My name is " + name);
+	}
+	
+	public void Testing() {
+		System.out.println("My salary is " + salary);
+	}
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Instance2 i2 = new Instance2();
+		
+		i2.id=89;
+		i2.name="WBT";
+		i2.salary=100000;
+		
+		i2.ADO();
+		i2.Software();
+		i2.Testing();
+		
+
+	}
+
+}
