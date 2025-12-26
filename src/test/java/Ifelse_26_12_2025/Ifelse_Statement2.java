bda96535cb2aefa994c535fadcc933acb77c3d14
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-26 10:04:31 +0530

    Ifelse2

---
+++ src/test/java/Ifelse_26_12_2025/Ifelse_Statement2.java
@@ -0,0 +1,19 @@
+package Ifelse_26_12_2025;
+
+public class Ifelse_Statement2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number = 18;
+		
+		if(number % 9 ==2 ) {
+			System.out.println("Even Number");
+		
+		}else {
+			System.out.println("Odd number");
+		}
+
+	}
+
+}
