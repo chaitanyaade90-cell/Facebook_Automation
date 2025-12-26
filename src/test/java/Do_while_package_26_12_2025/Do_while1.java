6790e3e715d2c68e01ddf2efcabee0906acc1ed4
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-26 12:36:51 +0530

    DoLoop1

---
+++ src/test/java/Do_while_package_26_12_2025/Do_while1.java
@@ -0,0 +1,19 @@
+package Do_while_package_26_12_2025;
+
+public class Do_while1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=1;
+		
+		do {
+			System.out.println(i);
+			
+			i++;
+			
+		}while (i >= 10);
+
+	}
+
+}
---
+++ src/test/java/Do_while_package_26_12_2025/package-info.java
@@ -0,0 +1,1 @@
+package Do_while_package_26_12_2025;
