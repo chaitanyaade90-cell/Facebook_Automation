38928a399767df84fe9615320e4b5fc793334442
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-27 10:09:15 +0530

    tryexception2

---
+++ src/test/java/Try_Catch_package_27_12_2025/Exception2.java
@@ -0,0 +1,25 @@
+package Try_Catch_package_27_12_2025;
+
+public class Exception2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 600;
+		int b = 300;
+		
+		try {
+			
+			int c = a/b;
+			System.out.println(c);
+		
+		}catch (ArithmeticException e) {
+			System.out.println("Intrest rate is 9%");
+		
+		}
+			
+		}
+
+	}
+
+
