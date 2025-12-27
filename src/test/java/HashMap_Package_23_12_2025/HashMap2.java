bef1ea3ba50cf7bbe9fccc23b87f7f2d5e93c6f0
chaitanyaade90-cell <chaitanya.ade90@gmail.com>
2025-12-27 13:52:38 +0530

    hashmap2

---
+++ src/test/java/HashMap_Package_23_12_2025/HashMap2.java
@@ -0,0 +1,20 @@
+package HashMap_Package_23_12_2025;
+
+import java.util.HashMap;
+
+public class HashMap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashMap<Integer, String> map = new HashMap<>();
+		
+		map.put(9000, "Regression");
+		map.put(18000, "Smoke");
+		map.put(27000, "WBT");
+		
+		System.out.println(map);
+
+	}
+
+}
